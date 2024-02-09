5af66983de6d5d4574cf29732c417edfaa66afd6
Sabrina Boc <98357578+sabrinugh@users.noreply.github.com>
2024-02-09 18:56:39 +0000

    Changing the main view to have a child container. Display map in child via view controller. Change in function layout.

--- developing_v2/MainViewController.swift
+++ developing_v2/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  developing_v2
 //
 //  Created by Sabrina Boc on 05/02/2024.
@@ -7,19 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    /*
     @IBAction func openMapView(_ sender: Any) {
         let vc = MapViewController()
-        vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: true, completion: nil);
     }
-    
+    */
 
 }
 
--- developing_v2/MapViewController.swift
+++ developing_v2/MapViewController.swift
@@ -12,28 +12,21 @@ import GooglePlaces
 class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad();
-        
-        /*
-            Instantiating the map options and setting up the map view onto the page
-            Directly setting the default camera position to Dublin City Centre via coordinates
-        */
         self.openMap();
         
     }
     
-    func openMap() {
+    /*
+    Instantiating the map options and setting up the map view onto the page
+    Directly setting the default camera position to Dublin City Centre via coordinates
+    */
+    private func openMap() {
         let options = GMSMapViewOptions()
         options.camera = GMSCameraPosition.camera(withLatitude: 53.34, longitude: 6.20, zoom: 6.0)
         options.frame = self.view.bounds
 
         let mapView = GMSMapView(options: options)
         self.view.addSubview(mapView)
-    }
-    
-    @IBAction func OpenHomePage(_ sender: Any) {
-        let vc = ViewController()
-        vc.modalPresentationStyle = .fullScreen
-        self.present(vc, animated: true, completion: nil);
     }
     
 }
