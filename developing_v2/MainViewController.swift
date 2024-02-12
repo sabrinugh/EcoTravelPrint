26a18ae171d5afba89ff304a262de29a2405b748
Sabrina Boc <98357578+sabrinugh@users.noreply.github.com>
2024-02-12 15:15:24 +0000

    Added a janky mapview. Still need to adjust the view container for the main view controller displaying the map.

--- developing_v2/MainViewController.swift
+++ developing_v2/MainViewController.swift
@@ -6,20 +6,40 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MainViewController: UIViewController {
-
+    //var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadMapView()
     }
     
-    /*
-    @IBAction func openMapView(_ sender: Any) {
-        let vc = MapViewController()
-        self.present(vc, animated: true, completion: nil);
+    @IBOutlet weak var mapViewSub: UIView!
+    /* Create the map View and add the Google Map into the UIView element. Load in any additional buttons onto the map view as self.view.addSubview() */
+    private func loadMapView() {
+        let options = GMSMapViewOptions()
+        options.camera = GMSCameraPosition.camera(withLatitude: 53.34, longitude: 6.20, zoom: 6.0)
+        options.frame = mapViewSub.bounds
+
+        let mapView = GMSMapView(options: options)
+        self.view.addSubview(mapView)
     }
-    */
+    
+    private func loadnavBar() {
+    }
+    
+    private func loadSearchBar() {
+        
+    }
+    
+    @IBOutlet weak var testButton: UIButton!
+    private func loadTestButton() {
+    
+    }
+    
 
 }
 
